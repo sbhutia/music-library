75829666915c0aeb914b359fbc357e1152eab564
wang <sbhutia13@gmail.com>
2017-04-22 19:03:16 +0000

    added disconnect

---
+++ web-service/src/server.js
@@ -0,0 +1,18 @@
+/*eslint-env node, express*/
+"use strict";
+var express = require ("express");
+var config = require("../config/config");
+var server = express();
+
+var Discogs = require("disconnect").Client;
+
+var db = new Discogs(config.discogsCredentials).database();
+
+server.get("/search", function(req, res){
+    db.search(req.query.q, {}, function(err, data){
+        console.log(err);
+        res.send(data);
+    });
+});
+
+server.listen("8080");
